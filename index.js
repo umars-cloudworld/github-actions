ef171704bbacfffd14ec51075ab45925d9cd4810
umars-cloudworld <programmingknowledge03@gmail.com>
2022-03-08 17:23:45 +0500

    resolved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda and Github! welcome"),
+    body: JSON.stringify("Hello from Lambda and Github! welcome g"),
   }
   return response
 }
